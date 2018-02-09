b2c1ce44931f2be9d63e200d1add95c5fdd8e0d8
Ubuntu <junchenj@VideoAnalytics7.jz0nigacmpoe5di5a2mbolttdf.bx.internal.cloudapp.net>
2018-02-09 03:58:52 +0000

    changing to cuda 9

--- code/tensorflow/install_tensorflow.sh
+++ code/tensorflow/install_tensorflow.sh
@@ -10,21 +10,30 @@ sudo apt-get install --assume-yes build-essential
 #wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
 #sudo sh cuda_8.0.61_375.26_linux-run --override
 
-wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
-sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+#wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+#sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install --assume-yes cuda
 
-echo "export PATH=/usr/local/cuda-8.0/bin:$PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
-export PATH=/usr/local/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+#echo "export PATH=/usr/local/cuda-8.0/bin:$PATH" >> ~/.bashrc
+#echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+#export PATH=/usr/local/cuda-8.0/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+echo "export PATH=/usr/local/cuda-9.0/bin:$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+export PATH=/usr/local/cuda-9.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
 
 #echo download and install cudnn libcudnn6_6.0.21-1+cuda8.0_amd64.deb!!!
-#scp junchenj@$server:~/libcudnn6_6.0.21-1+cuda8.0_amd64.deb .
-sudo dpkg -i libcudnn6_6.0.21-1+cuda8.0_amd64.deb
-echo "export CUDA_HOME=/usr/local/cuda-8.0" >> ~/.bashrc
-export CUDA_HOME=/usr/local/cuda-8.0
+#sudo dpkg -i libcudnn6_6.0.21-1+cuda8.0_amd64.deb
+sudo dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+#echo "export CUDA_HOME=/usr/local/cuda-8.0" >> ~/.bashrc
+echo "export CUDA_HOME=/usr/local/cuda-9.0" >> ~/.bashrc
+#export CUDA_HOME=/usr/local/cuda-8.0
+export CUDA_HOME=/usr/local/cuda-9.0
 
 sudo apt-get install --assume-yes libcupti-dev
 sudo apt-get install --assume-yes python-pip python-dev
